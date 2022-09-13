8a71120b4cd1618672b07f45217d95bbbc5b5ae2
mahadev koli <pragatimkoli0@gmail.com>
2022-09-13 12:40:55 +0530

    Added ArrayList Code

---
+++ MainProject/src/com/velocity/collections/ArrayListDemo.java
@@ -0,0 +1,24 @@
+package com.velocity.collections;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> al = new ArrayList<Integer>();
+		al.add(10);
+		al.add(20);
+		al.add(30);
+		al.add(40);
+		al.add(50);
+		
+		Iterator<Integer> it = al.iterator();
+		while(it.hasNext())
+		{
+			System.out.println(it.next());
+		}
+
+	}
+
+}
