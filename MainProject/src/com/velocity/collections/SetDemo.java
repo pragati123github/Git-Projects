c606f45b499acc5b5bfc8413b230173a64a338bf
mahadev koli <pragatimkoli0@gmail.com>
2022-09-13 13:06:18 +0530

    Added Code For Set

---
+++ MainProject/src/com/velocity/collections/SetDemo.java
@@ -0,0 +1,22 @@
+package com.velocity.collections;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetDemo {
+
+	public static void main(String[] args) {
+		Set<Integer> set = new HashSet<Integer>();
+		set.add(10);
+		set.add(20);
+		set.add(30);
+		set.add(40);
+		
+		for(int i:set)
+		{
+			System.out.println(i);
+		}
+
+	}
+
+}
